3e8207ed51af0d497b4047b7520697a804434f5c
macremote <>
2021-04-12 14:31:17 +0800

    项目初始化

---
+++ Shunyi/Shunyi/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  Shunyi
+//
+//  Created by macremote on 2021/4/12.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ Shunyi/Shunyi/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  Shunyi
+//
+//  Created by macremote on 2021/4/12.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
